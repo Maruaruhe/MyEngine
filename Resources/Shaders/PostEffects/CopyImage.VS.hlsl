e30fc3bc643796d03d7cd485b04d70e4c980a060
k022g0101@g.neec.ac.jp <k022g0101@g.neec.ac.jp>
2024-06-06 12:41:46 +0900

    Shader

--- Resources/Shaders/PostEffects/CopyImage.VS.hlsl
+++ Resources/Shaders/PostEffects/CopyImage.VS.hlsl
@@ -1,4 +1,25 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+#include "CopyImage.hlsli"
+
+static const uint32_t kNumVertex = 3;
+static const float32_t4 kPositions[kNumVertex] =
 {
-	return pos;
+    { -1.0f, 1.0f, 0.0f, 1.0f },    //LeftTop
+    { 3.0f, 1.0f, 0.0f, 1.0f },     //RightTop
+    { -1.0f, -3.0f, 0.0f, 1.0f },  //LeftBot
+};
+
+static const float32_t2 kTexcoords[kNumVertex] =
+{
+    { 0.0f, 0.0f },   //LeftTop
+    { 2.0f, 0.0f },  //RightTop
+    { 0.0f, 2.0f },  //LeftBot
+};
+
+
+VertexShaderOutput main(uint32_t vertexId : SV_VertexID)
+{
+    VertexShaderOutput output;
+    output.position = kPositions[vertexId];
+    output.texcoord = kTexcoords[vertexId];
+    return output;
 }
