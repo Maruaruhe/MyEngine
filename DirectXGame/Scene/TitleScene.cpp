34d1a48db5cd5ba056271120d8645d7a83f4cf1a
aruma <k022g0101@g.neec.ac.jp>
2024-09-02 08:28:33 +0900

    Title

--- DirectXGame/Scene/TitleScene.cpp
+++ DirectXGame/Scene/TitleScene.cpp
@@ -4,36 +4,18 @@
 void TitleScene::Initialize() {
 	input = KeyInput::GetInstance();
 
-	camera2 = std::make_unique<Camera>();
-	camera2->Initialize();
-	camera2->transform.translate.z = -15.0f;
-
-	ModelManager::GetInstance()->LoadModel("a");
-	model.Initialize("a");
-	model.transform.rotate.y = 1.57f;
-	model.transform.translate.y -= 1.5f;
-
-	TextureManager::GetInstance()->LoadTexture("Resources/Map/wall.png");
-	model.textureIndex = TextureManager::GetInstance()->GetTextureIndexByFilePath("Resources/Map/wall.png");
-
-	model.SetCamera(camera2.get());
-
-	TextureManager::GetInstance()->LoadTexture("Resources/ao.png");
-	title.Initialize({0,0}, {320,180}, "Resources/ao.png");
+	TextureManager::GetInstance()->LoadTexture("Resources/Title/title.png");
+	title.Initialize({0,0}, {1280,720}, "Resources/Title/title.png");
 }
 
 void TitleScene::Update() {
-	camera2->Update();
-	model.Update();
-
 	title.Update();
 
-	if (KeyInput::GetInstance()->PushKey(DIK_A)) {
+	if (KeyInput::GetInstance()->PushKey(DIK_SPACE)) {
 		sceneNo = INGAME;
 	}
 }
 
 void TitleScene::Draw() {
-	model.Draw();
 	title.Draw();
 }
